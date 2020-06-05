6453a14e2934534c111756d6aed33a85b139373c
David Hutchison <dhutchis68516@gmail.com>
2020-06-05 17:04:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,13 +51,12 @@ class Student
 
   def self.new_from_db(row)
     binding.pry
-    new_student = Student.new
+    new_student = self.new(row[0], row[1], row[2])
     binding.pry
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
     new_student
-    binding.pry
   end
 
   def self.find_by_name(name)
