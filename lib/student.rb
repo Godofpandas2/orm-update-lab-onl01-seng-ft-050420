5c4cd8a21c34a6b86519181eebd3281a6ed0f7b8
David Hutchison <dhutchis68516@gmail.com>
2020-06-05 17:10:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,4 +66,9 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def update
+    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
 end
